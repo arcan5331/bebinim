273c193ca8dcf5dd716756828812f2e94a9a80ee
arcan5331 <alighaforian@yahoo.com>
2024-04-25 17:58:24 +0330

    added basic user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'phone_number' => '09212926290',
+            'name' => 'name',
+            'password' => 1234
         ]);
     }
 }
